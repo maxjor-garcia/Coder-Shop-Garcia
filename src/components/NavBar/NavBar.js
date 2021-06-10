6f8f5f2401626e6ae20cabae3cebd87daf38d9bc
Maximiliano Jorge Garcia <maxjor.garcia@gmail.com>
2021-06-10 19:54:52 -0300

    se creo navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+export const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Consolas</a></li>
+                <li><a href="#">Juegos</a></li>
+                <li><a href="#">Accesorios</a></li>
+            </ul>
+        </nav>
+    )
+}
